c6ce1f011cb81b2c2034e99cb4729c7c643f8f65
nineclicks <nicholasg1989@gmail.com>
2017-12-23 21:02:16 -0800

    Move files to spiffs

--- wifibutton.ino
+++ wifibutton.ino
@@ -1,24 +1,42 @@
 #include <WiFiClient.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
+#include <FS.h>
 
 const char *ssid = "ButtonSetup";
 
 ESP8266WebServer server(80);
 
-void rootRoute() {
-  String rootHtml = "::index.html::";
-  server.send(200, "text/html", rootHtml);
+String getContentType(String filename) {
+  if (server.hasArg("download")) return "application/octet-stream";
+  else if (filename.endsWith(".htm")) return "text/html";
+  else if (filename.endsWith(".html")) return "text/html";
+  else if (filename.endsWith(".css")) return "text/css";
+  else if (filename.endsWith(".js")) return "application/javascript";
+  else if (filename.endsWith(".png")) return "image/png";
+  else if (filename.endsWith(".gif")) return "image/gif";
+  else if (filename.endsWith(".jpg")) return "image/jpeg";
+  else if (filename.endsWith(".ico")) return "image/x-icon";
+  else if (filename.endsWith(".xml")) return "text/xml";
+  else if (filename.endsWith(".pdf")) return "application/x-pdf";
+  else if (filename.endsWith(".zip")) return "application/x-zip";
+  else if (filename.endsWith(".gz")) return "application/x-gzip";
+  return "text/plain";
 }
 
-void scriptRoute() {
-  String script = "::script.js::";
-  server.send(200, "text/javascript", script);
-}
-
-void styleRoute() {
-  String script = "::style.css::";
-  server.send(200, "text/css", script);
+bool handleFileRead(String path) {
+  if (path.endsWith("/")) path += "index.html";
+  String contentType = getContentType(path);
+  String pathWithGz = path + ".gz";
+  if (SPIFFS.exists(pathWithGz) || SPIFFS.exists(path)) {
+    if (SPIFFS.exists(pathWithGz))
+      path += ".gz";
+    File file = SPIFFS.open(path, "r");
+    size_t sent = server.streamFile(file, contentType);
+    file.close();
+    return true;
+  }
+  return false;
 }
 
 void scanRoute() {
@@ -29,7 +47,7 @@ void scanRoute() {
     if (rssi > 100) rssi = 100;
     if (rssi < 0) rssi = 0;
     String ssid = WiFi.SSID(i);
-    ssid.replace("\"","\\\"");
+    ssid.replace("\"", "\\\"");
     ssids += (String) "  {\n    \"ssid\": \"" + ssid + "\",\n";
     ssids += (String) "    \"secured\": " + (WiFi.encryptionType(i) != ENC_TYPE_NONE) + ",\n";
     ssids += (String) "    \"signal\": " + rssi + ",\n";
@@ -49,14 +67,16 @@ void connectRoute() {
 void setup() {
   delay(1000);
   WiFi.softAP(ssid);
-
-  server.on("/", rootRoute);
+  SPIFFS.begin();
   server.on("/scan", scanRoute);
-  server.on("/script.js", scriptRoute);
-  server.on("/style.css", styleRoute);
   server.on("/connect", HTTP_POST, connectRoute);
   server.begin();
   Serial.begin(115200);
+
+  server.onNotFound([]() {
+    if (!handleFileRead(server.uri()))
+      server.send(404, "text/plain", "FileNotFound");
+  });
 }
 
 void loop() {
